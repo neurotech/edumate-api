2a8b2523467e7901d762f2d03243445486203683
Tim Douglas <neurotech@gmail.com>
2015-02-10 15:52:53 +1100

    Experimenting with hapi

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-var config = require('./config');
+var config = require('./config'),
+    Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.start(function () {
+  console.log('Server running at:', server.info.uri);
+});
