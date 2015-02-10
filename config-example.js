c60d214444d8999a1a92bea97f579860ad36043c
Tim Douglas <neurotech@gmail.com>
2015-02-10 15:40:19 +1100

    Added example config.js

---
+++ config-example.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var config = {};
+
+config.express = {
+  host: 'http://localhost',
+  port: 3333
+};
+
+config.db2 = {
+  host: 'HOSTNAME',
+  port: 'PORT',
+  suffix: '/SUFFIX',
+  username: 'USERNAME',
+  password: 'SECRET'
+};
+
+config.init = {
+  libpath: './drivers/db2jcc.jar',
+  drivername: 'com.ibm.db2.jcc.DB2Driver',
+  url: 'jdbc:' + 'db2://' + config.db2.host + ':' + config.db2.port + config.db2.suffix + ':user=' + config.db2.username + ';password=' + config.db2.password + ';'
+};
+
+config.cache = {
+    value: 5,
+    units: 'minutes',
+    path: path.join(__dirname, '/db/')
+};
+
+module.exports = config;
