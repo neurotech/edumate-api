d94e49c0e5ca6ea4343d072cd53d174be58ff1bb
Tim Douglas <neurotech@gmail.com>
2015-05-27 18:56:50 +1000

    Simple app.js to query staff_users view

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// TBD
+'use strict';
+
+var edumate = require('node-edumate');
+var config = require('./config.js');
+
+var staffUsers = 'SELECT * FROM DB2INST1.view_api_v1_staff_users';
+
+edumate.query(staffUsers, config.init).then(function(results) {
+  // Do something with results
+  console.log(results);
+}, function(error) {
+  console.error(error);
+});
