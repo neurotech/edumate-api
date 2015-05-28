f22fe8f91b71158a11db43674e25d3125162939e
Tim Douglas <neurotech@gmail.com>
2015-05-28 17:13:18 +1000

    Prototyping with lowed

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 'use strict';
 
 var edumate = require('node-edumate');
-var config = require('./config.js');
+var low = require('lowdb');
+var _ = require('lodash');
+var camelCase = require('camel-case');
+var db = low('db.json');
 
+var config = require('./config.js');
 var staffUsers = 'SELECT * FROM DB2INST1.view_api_v1_staff_users';
 
 edumate.query(staffUsers, config.init).then(function(results) {
-  // Do something with results
-  console.log(results);
+  db('staff').remove();
+  for (var i = 0; i < results.length; i++) {
+    db('staff').push(
+      _.transform(results[i], function (result, val, key) {
+        result[camelCase(key)] = val;
+      })
+    );
+  };
 }, function(error) {
   console.error(error);
 });
