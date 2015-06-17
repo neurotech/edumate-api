9dec72609b8b5dfbe759598a7129ba238662e380
Tim Douglas <neurotech@gmail.com>
2015-06-17 22:36:55 +1000

    Renamed insertResults to insertTable

--- setup-records.js
+++ setup-records.js
@@ -10,7 +10,7 @@ var config = require('./config');
 Object.keys(queries).map(function(key) {
   var value = queries[key];
   edumate.query(value.sql, config.init).then(function(results) {
-    rethinkdb.insertResults(value.name, results);
+    rethinkdb.insertTable(value.name, results);
   }, function(error) {
     console.error(error);
   });
