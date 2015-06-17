f2fe643d34458878c0e56f6aa74a05ecb8692ef8
Tim Douglas <neurotech@gmail.com>
2015-06-17 20:58:39 +1000

    Added moduleReports timetable job

--- app.js
+++ app.js
@@ -7,5 +7,6 @@ var queries = require('./lib/queries');
 
 timetable.updateJob(queries.staff.name, queries.staff.sql, queries.staff.schedule);
 timetable.updateJob(queries.staffAbsent.name, queries.staffAbsent.sql, queries.staffAbsent.schedule);
+timetable.updateJob(queries.moduleReports.name, queries.moduleReports.sql, queries.moduleReports.schedule);
 
 server.start();
