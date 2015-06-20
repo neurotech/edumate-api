c535faacdf2ca23f67442e02ca310ae69bb50fd0
Tim Douglas <neurotech@gmail.com>
2015-06-20 16:11:48 +1000

    Separated API and toolbelt routing

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 
 var server = require('./lib/hapi');
 
-var timetable = require('./lib/timetable');
-var queries = require('./lib/queries');
+var timetable = require('./lib/api/timetable');
+var queries = require('./lib/api/queries');
 
-timetable.updateJob(queries.staff.name, queries.staff.sql, queries.staff.schedule);
-timetable.updateJob(queries.staffAbsent.name, queries.staffAbsent.sql, queries.staffAbsent.schedule);
-timetable.updateJob(queries.moduleReports.name, queries.moduleReports.sql, queries.moduleReports.schedule);
+timetable.replaceJob(queries.staff.name, queries.staff.sql, queries.staff.schedule);
+timetable.replaceJob(queries.moduleReports.name, queries.moduleReports.sql, queries.moduleReports.schedule);
+//timetable.replaceJob(queries.staffAbsent.name, queries.staffAbsent.sql, queries.staffAbsent.schedule);
+timetable.replaceJob(queries.periods.name, queries.periods.sql, queries.periods.schedule)
 
 server.start();
