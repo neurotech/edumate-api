d8b8a66bdfec9a4f7dac70db653ae5134c5e7a5b
Tim Douglas <neurotech@gmail.com>
2015-06-23 17:17:17 +1000

    Added front-end basics including vue.js experimentation

---
+++ client/js/client.js
@@ -0,0 +1,24 @@
+var Vue = require('vue');
+
+var apiUrl = '/api/reports/all'
+
+new Vue({
+    el: '#reports',
+    data: {
+      reports: null
+    },
+    created: function () {
+      this.fetchData();
+    },
+    methods: {
+      fetchData: function () {
+        var xhr = new XMLHttpRequest(),
+        self = this
+        xhr.open('GET', apiUrl)
+        xhr.onload = function () {
+          self.reports = JSON.parse(xhr.responseText);
+        }
+        xhr.send()
+      }
+    }
+});
