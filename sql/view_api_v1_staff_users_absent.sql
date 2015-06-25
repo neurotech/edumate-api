f6e33614cc0e6516c000e2231e8e3cc4556ecf4e
Tim Douglas <neurotech@gmail.com>
2015-06-25 14:20:09 +1000

    Separated from_date/to_date to date and time elements, added sort key

--- sql/view_api_v1_staff_users_absent.sql
+++ sql/view_api_v1_staff_users_absent.sql
@@ -1,26 +1,28 @@
 -- GRANT SELECT ON "DB2INST1"."VIEW_API_V1_STAFF_USERS_ABSENT" TO USER API
 
 CREATE OR REPLACE VIEW DB2INST1.VIEW_API_V1_STAFF_USERS_ABSENT (
+  sort_key,
   id,
   away_reason,
-  from_date,
-  to_date
+  date_from,
+  time_from,
+  date_to,
+  time_to
 ) AS
 
-SELECT * FROM (
-  SELECT
-    staff.staff_number AS ID,
-    REPLACE(away_reason.away_reason, '&#039;', '''') AS AWAY_REASON,
-    from_date,
-    to_date
-  
-  FROM staff_away
-  
-  INNER JOIN staff ON staff.staff_id = staff_away.staff_id
-  INNER JOIN contact ON contact.contact_id = staff.contact_id
-  INNER JOIN away_reason ON away_reason.away_reason_id = staff_away.away_reason_id
-  
-  WHERE (current date) BETWEEN DATE(from_date) AND DATE(to_date)
-  
-  ORDER BY DATE(from_date) DESC, REPLACE(away_reason.away_reason, '&#039;', ''''), UPPER(contact.surname), contact.preferred_name, contact.firstname
-)
+SELECT
+  (ROW_NUMBER() OVER (ORDER BY DATE(from_date) DESC, TIME(from_date) DESC, REPLACE(away_reason.away_reason, '&#039;', ''''), UPPER(contact.surname), contact.preferred_name, contact.firstname) + 99) AS "SORT_KEY",
+  staff.staff_number AS ID,
+  REPLACE(away_reason.away_reason, '&#039;', '''') AS AWAY_REASON,
+  DATE(from_date) AS "DATE_FROM",
+  TIME(from_date) AS "TIME_FROM",
+  DATE(to_date) AS "DATE_TO",
+  TIME(to_date) AS "TIME_TO"
+
+FROM staff_away
+
+INNER JOIN staff ON staff.staff_id = staff_away.staff_id
+INNER JOIN contact ON contact.contact_id = staff.contact_id
+INNER JOIN away_reason ON away_reason.away_reason_id = staff_away.away_reason_id
+
+WHERE (current date) BETWEEN DATE(from_date) AND DATE(to_date)
