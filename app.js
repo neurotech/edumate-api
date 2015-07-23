a54a024099660dc2a3a219e7e5da58c4c0000da3
Tim Douglas <neurotech@gmail.com>
2015-07-23 17:23:50 +1000

    Large refactor

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 'use strict';
 
-var server = require('./lib/hapi');
+// var server = require('./lib/hapi');
 
 var timetable = require('./lib/api/timetable');
 var queries = require('./lib/api/queries');
+
+/*
+ * TODO:
+ * -----
+ * Replicate the code from `edumate-canvas-sync/app.js` to handle iteration below
+*/
 
 timetable.replaceJob(queries.staff.name, queries.staff.sql, queries.staff.schedule);
 timetable.replaceJob(queries.moduleReports.name, queries.moduleReports.sql, queries.moduleReports.schedule);
 timetable.replaceJob(queries.staffAbsent.name, queries.staffAbsent.sql, queries.staffAbsent.schedule);
 timetable.replaceJob(queries.periods.name, queries.periods.sql, queries.periods.schedule);
 
-server.start();
+// server.start();
