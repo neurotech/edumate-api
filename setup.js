78c71fa351e646dc507d65e0e869f494803bc3e7
Tim Douglas <neurotech@gmail.com>
2015-08-12 22:07:28 +1000

    Updated setup.js to setup and populate JWT auth/users elements

--- setup.js
+++ setup.js
@@ -5,11 +5,16 @@ require('rethinkdb-init')(r);
 
 var edumate = require('node-edumate');
 var async = require('async');
+var jwt = require('jsonwebtoken');
+var moment = require('moment');
 var readline = require('readline');
 var chalk = require('chalk');
 
 var queries = require('./lib/api/queries');
 var config = require('./config');
+
+var now = moment().format();
+var token = jwt.sign({ user: 'picnic', generated: now }, config.secret, { algorithm: 'HS512' });
 
 var rl = readline.createInterface({
   input: process.stdin,
@@ -35,6 +40,36 @@ async.series([
       callback(null, 'begin!');
     });
   },
+  function secretSkeleton (callback) {
+    r.init({
+        host: 'localhost',
+        port: 28015,
+        db: 'auth'
+      }, ['users'])
+      .then(function (conn) {
+        console.log(chalk.magenta('[rethinkdb] ') + 'Created db: ' + chalk.green('auth') + ' and table: ' + chalk.green('users'));
+        callback(null, 'token');
+      });
+  },
+  function secret (callback) {
+    r.db('auth')
+      .table('users')
+      .insert({
+        user: 'picnic',
+        generated: now,
+        token: token
+      })
+      .then(function (result) {
+        var tokenSize = function sizeHandler (token) {
+          if (token.length >= 1024) {
+            return (token.length / 1024) + ' KB';
+          } else {
+            return token.length + ' bytes';
+          }
+        };
+        console.log(chalk.yellow('[jwt] ') + 'Token generated (' + chalk.red(tokenSize(token)) + ') and written to the ' + chalk.green('users') + ' table in the ' + chalk.green('auth') + ' db.');
+      });
+  },
   function skeleton (callback) {
     r.init({
         host: 'localhost',
@@ -42,7 +77,7 @@ async.series([
         db: 'edumate_toolbelt'
       }, manifest(queries))
       .then(function (conn) {
-        console.log(chalk.magenta('[rethinkdb] ') + 'Created db ' + chalk.green('edumate_toolbelt') + ' with tables: ');
+        console.log(chalk.magenta('[rethinkdb] ') + 'Created db: ' + chalk.green('edumate_toolbelt') + ' with tables: ');
         console.log(chalk.magenta('[rethinkdb] ') + manifest(queries));
         callback(null, 'skeleton');
       }
