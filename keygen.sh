e202f8be7ceb1ef655f364ec48bccb1919d816c9
Tim Douglas <neurotech@gmail.com>
2015-08-14 15:59:16 +1000

    Updated keygen.sh

--- keygen.sh
+++ keygen.sh
@@ -1,9 +1,17 @@
-echo "Generating self-signed cert in ./private"
-mkdir private
+echo "Generating cert in ./private"
+echo "Note: Make sure you specify the correct domain!"
 
-openssl genrsa -des3 -passout pass:x -out private/server.pass.key 2048
-openssl rsa -passin pass:x -in private/server.pass.key -out private/server.key
-rm private/server.pass.key
 openssl req -new -key private/server.key -out private/server.csr
-
 openssl x509 -req -days 365 -in private/server.csr -signkey private/server.key -out private/server.crt
+
+# echo "Generating self-signed cert in ./private"
+# mkdir private
+#
+# openssl genrsa -des3 -passout pass:x -out private/server.pass.key 2048
+# openssl rsa -passin pass:x -in private/server.pass.key -out private/server.key
+# rm private/server.pass.key
+
+# Generated version
+# openssl req -new -key private/server.key -out private/server.csr
+#
+# openssl x509 -req -days 365 -in private/server.csr -signkey private/server.key -out private/server.crt
