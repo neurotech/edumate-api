54abc749710688cec40b41a4861a6394b99001d0
Tim Douglas <neurotech@gmail.com>
2015-08-20 14:49:42 +1000

    Separated routes for /staff/absent
    /api/staff/absent/now
    /api/staff/absent/soon
    /api/staff/absent/allday
    /api/staff/absent/today

--- sql/view_api_v1_staff_users_absent.sql
+++ sql/view_api_v1_staff_users_absent.sql
@@ -8,23 +8,42 @@ CREATE OR REPLACE VIEW DB2INST1.VIEW_API_V1_STAFF_USERS_ABSENT (
   date_from,
   time_from,
   date_to,
-  time_to
+  time_to,
+  all_day_flag
 ) AS
 
+WITH current_aways AS (
+  SELECT
+    staff_away.staff_away_id AS ID,
+    staff.staff_number AS STAFF_ID,
+    contact.preferred_name,
+    contact.firstname,
+    contact.surname,
+    REPLACE(away_reason.away_reason, '&#039;', '''') AS AWAY_REASON,
+    DATE(from_date) AS "DATE_FROM",
+    TIME(from_date) AS "TIME_FROM",
+    DATE(to_date) AS "DATE_TO",
+    TIME(to_date) AS "TIME_TO",
+    (CASE WHEN TIME(from_date) = '01:00:00' AND TIME(to_date) = '23:59:00' THEN 1 ELSE 0 END) AS "ALL_DAY_FLAG"
+  
+  FROM staff_away
+  
+  INNER JOIN staff ON staff.staff_id = staff_away.staff_id
+  INNER JOIN contact ON contact.contact_id = staff.contact_id
+  INNER JOIN away_reason ON away_reason.away_reason_id = staff_away.away_reason_id
+  
+  WHERE (current date) BETWEEN DATE(from_date) AND DATE(to_date)
+)
+
 SELECT
-  (ROW_NUMBER() OVER (ORDER BY DATE(from_date) DESC, TIME(from_date) DESC, REPLACE(away_reason.away_reason, '&#039;', ''''), UPPER(contact.surname), contact.preferred_name, contact.firstname) + 99) AS "SORT_KEY",
-  staff_away.staff_away_id AS ID,
-  staff.staff_number AS STAFF_ID,
-  REPLACE(away_reason.away_reason, '&#039;', '''') AS AWAY_REASON,
-  DATE(from_date) AS "DATE_FROM",
-  TIME(from_date) AS "TIME_FROM",
-  DATE(to_date) AS "DATE_TO",
-  TIME(to_date) AS "TIME_TO"
+  (ROW_NUMBER() OVER (ORDER BY date_from DESC, all_day_flag, time_from ASC, away_reason, UPPER(surname), preferred_name, firstname) + 99) AS "SORT_KEY",
+  id,
+  staff_id,
+  away_reason,
+  date_from,
+  time_from,
+  date_to,
+  time_to,
+  all_day_flag
 
-FROM staff_away
-
-INNER JOIN staff ON staff.staff_id = staff_away.staff_id
-INNER JOIN contact ON contact.contact_id = staff.contact_id
-INNER JOIN away_reason ON away_reason.away_reason_id = staff_away.away_reason_id
-
-WHERE (current date) BETWEEN DATE(from_date) AND DATE(to_date)
+FROM current_aways
