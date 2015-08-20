ecee0dea51358bf1c17ae3be449b5ecfc83799b7
Tim Douglas <neurotech@gmail.com>
2015-08-20 11:22:30 +1000

    More experimentation with keygen.sh

--- keygen.sh
+++ keygen.sh
@@ -1,17 +1,16 @@
-echo "Generating cert in ./private"
-echo "Note: Make sure you specify the correct domain!"
+# echo "Generating cert in ./private"
+# echo "Note: Make sure you specify the correct domain!"
 
-openssl req -new -key private/server.key -out private/server.csr
-openssl x509 -req -days 365 -in private/server.csr -signkey private/server.key -out private/server.crt
+# openssl req -new -key private/server.key -out private/server.csr
+# openssl x509 -req -days 365 -in private/server.csr -signkey private/server.key -out private/server.crt
 
 # echo "Generating self-signed cert in ./private"
 # mkdir private
 #
-# openssl genrsa -des3 -passout pass:x -out private/server.pass.key 2048
-# openssl rsa -passin pass:x -in private/server.pass.key -out private/server.key
-# rm private/server.pass.key
+openssl genrsa -des3 -passout pass:x -out private/server.pass.key 2048
+openssl rsa -passin pass:x -in private/server.pass.key -out private/server.key
+rm private/server.pass.key
 
 # Generated version
-# openssl req -new -key private/server.key -out private/server.csr
-#
-# openssl x509 -req -days 365 -in private/server.csr -signkey private/server.key -out private/server.crt
+openssl req -new -key private/server.key -out private/server.csr
+openssl x509 -req -days 365 -in private/server.csr -signkey private/server.key -out private/server.crt
