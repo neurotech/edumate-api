63aa67b18d8c58228fa720c1d12fc67f990e5286
Tim Douglas <neurotech@gmail.com>
2016-02-09 21:34:13 +1100

    Tidied up app.js logging

--- app.js
+++ app.js
@@ -10,16 +10,23 @@ const config = require('./config');
 server.start(function () {
   var today = chalk.magenta(moment().format('DD/MM/YY'));
   var now = chalk.yellow(moment().format('HH:mm:ss'));
+  var api = 'http://' + config.http.host + ':' + config.http.port;
+  var edumateString = process.env.EDUMATE_USERNAME + '@' + process.env.EDUMATE_HOST + ':' + process.env.EDUMATE_PORT + '/' + process.env.EDUMATE_PATH;
 
-  console.log('Starting ' + chalk.green('edumate-api') + ' on ' + today + ' at ' + now);
-  console.log(chalk.cyan('API: ') + chalk.underline('http://' + config.http.host + ':' + config.http.port));
-  console.log(chalk.blue('Edumate: ') + process.env.EDUMATE_USERNAME + '@' + process.env.EDUMATE_HOST + ':' + process.env.EDUMATE_PORT + '/' + process.env.EDUMATE_PATH);
+  // Log a basic startup message
+  console.log(
+    `
+      Starting ${chalk.green('edumate-api')} on ${today} at ${now}.
+      ${chalk.cyan('API: ')} ${chalk.underline(api)}
+      ${chalk.blue('Edumate: ')} ${chalk.white(edumateString)}
+    `
+  );
 
   // Iterate over `./lib/api/queries` and pass each one to ``./lib/api/timetable`
   for (var key in queries) {
     if (queries.hasOwnProperty(key)) {
       timetable.replaceJob(queries[key].dataset, queries[key].sql, queries[key].schedule);
-      console.log(chalk.red('Scheduled job: ') + queries[key].dataset);
+      console.log(`${chalk.red('Scheduled job: ')} ${queries[key].dataset}`);
     }
   }
 });
