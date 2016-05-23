c29fa8d162967fb3e7a021f44f83c927453bca62
Tim Douglas <neurotech@gmail.com>
2016-05-23 16:50:07 +1000

    Added hapi-auth-jwt2 for authentication

--- app/lib/hapi.js
+++ app/lib/hapi.js
@@ -3,24 +3,26 @@
 const hapi = require('hapi');
 const config = require('../config');
 const apiRoutes = require('./api/routes');
+const auth = require('./auth');
 
 var server = new hapi.Server();
 
 server.connection({
   port: config.http.port,
-  router: { stripTrailingSlash: true },
-  routes: { cors: true }
+  router: { stripTrailingSlash: true }
 });
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: (request, reply) => {
-    var nope = { response: 'nope' };
-    reply(nope);
-  }
-});
+server.register(require('hapi-auth-jwt2'), (err) => {
+  if (err) { console.error(err); }
 
-server.route(apiRoutes);
+  server.auth.strategy('jwt', 'jwt', {
+    key: config.auth.secret,
+    validateFunc: auth.validate,
+    verifyOptions: { algorithms: [ 'HS256' ] }
+  });
+
+  server.auth.default('jwt');
+  server.route(apiRoutes);
+});
 
 module.exports = server;
